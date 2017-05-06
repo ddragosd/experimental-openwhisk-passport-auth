c3b4b7fb6dc44653bf1be4620d744007c24fe919
Dragos Dascalita Haut <ddragosd@gmail.com>
2017-05-05 17:21:29 -0700

    tested with Twitter and GitHub and updated the code for OAuth 1 too

--- src/action/auth.js
+++ src/action/auth.js
@@ -1,17 +1,33 @@
 import passport from 'passport'
-import passport_facebook from 'passport-facebook'
-import passport_google from 'passport-google'
 import passport_github from 'passport-github'
-
+import passport_facebook from 'passport-facebook'
+import passport_twitter  from 'passport-twitter'
+import passport_google from 'passport-google'
 
 function _authenticate(params) {
     return new Promise((resolve, reject) => {
 
-        let strategy_impl = require('passport-' + params.auth_provider).Strategy;
+        let passport_module_name = 'passport-' + params.auth_provider;
+
+        let strategy_impl = null;
+
+        try {
+            strategy_impl = require(passport_module_name).Strategy;
+        } catch (err) {
+            console.log(err);
+            reject({
+                    "message": "Could not load " + passport_module_name,
+                    "error": err.toString()
+                }
+            );
+        }
+
 
         let strategy = new strategy_impl({
             clientID: params.client_id,
+            consumerKey: params.client_id,
             clientSecret: params.client_secret,
+            consumerSecret: params.client_secret,
             callbackURL: params.callback_url
         }, function (accessToken, refreshToken, profile, done) {
             console.log("Logged in successfully ... ");
@@ -25,12 +41,26 @@ function _authenticate(params) {
         });
 
 
-        // a lightweight request object to be used in this serverless context
+        // a lightweight request object to be used in the serverless context
         let request = {
-            query: params // expose query parameters
+            query: params,     // expose query parameters
+            session: strategy._requestTokenStore || strategy._stateStore // inherit the session from Passport
         };
 
-        // a lightweight response object to be used in this serverless context
+        if (strategy._requestTokenStore) { // OAuth 1 requires a session
+            strategy._requestTokenStore.get = function(req, token, cb) {
+                // NOTE: The oauth_verifier parameter will be supplied in the query portion
+                //       of the redirect URL, if the server supports OAuth 1.0a.
+                var oauth_verifier = req.query.oauth_verifier || null;
+                return cb(null, oauth_verifier);
+            }
+            strategy._requestTokenStore.destroy = function(req, token, cb) {
+                // simply invoke the callback directly
+                cb();
+            }
+        }
+
+        // a lightweight response object to be used in the serverless context
         let response = {
             headers: {},
             setHeader: function (name, val) {
@@ -43,6 +73,10 @@ function _authenticate(params) {
         };
 
         let get_action_response = function (resp) {
+            if (resp.body instanceof Error) {
+                console.error(resp.body);
+                resp.body = resp.body.toString();
+            }
             return {
                 headers: resp.headers,
                 statusCode: resp.statusCode,
@@ -59,13 +93,15 @@ function _authenticate(params) {
 
         passport.use(strategy);
 
-        let scopes = params.scopes || "";
-        scopes = scopes.split(",");
+        let scopes = params.scopes || null;
+        if ( scopes !== null ) {
+            scopes = scopes.split(",");
+        }
 
         let res = passport.authenticate(params.auth_provider, {
-            scope: scopes , //['user_posts', 'publish_actions'],
-            successRedirect: '/success',  // TODO:  TBD should this is read from the parameters ?
-            failureRedirect: '/login'     // TODO: TBD should this is read from the parameters ?
+            scope: scopes,
+            successRedirect: '/success',  // TODO: TBD should this be read from parameters ?
+            failureRedirect: '/login'     // TODO: TBD should this be read from parameters ?
         });
 
         res(request, response, next);
--- test/auth.spec.js
+++ test/auth.spec.js
@@ -12,7 +12,30 @@ chai.should();
 describe('OAuth2 Auth', () => {
     describe('with a provider', () => {
 
-        it('should redirect to the login page', (done) => {
+        it('GitHub: should redirect to the login page', (done) => {
+            var params = {
+                auth_provider: "github",
+                client_id: "test_client_id",
+                client_secret: "test_client_secret",
+                scopes: "user_posts,publish_actions",
+                callback_url: "http://www.example.com/auth/facebook/callback"
+            };
+
+            // The action returns a Promise and we can use "eventually" to wait for it.
+            // If the action doesn't return a Promise we can remove "eventually"
+            //   and write instead "should.deep.equal"
+            var result = action(params);
+            result.should.eventually.deep.equal({
+                headers: {
+                    Location: 'https://github.com/login/oauth/authorize?response_type=code&redirect_uri=http%3A%2F%2Fwww.example.com%2Fauth%2Ffacebook%2Fcallback&scope=user_posts%2Cpublish_actions&client_id=test_client_id',
+                    'Content-Length': '0'
+                },
+                statusCode: 302,
+                body: ''
+            }).notify(done);
+        });
+
+        it('FB: should redirect to the login page', (done) => {
             var params = {
                 auth_provider: "facebook",
                 client_id: "test_client_id",
